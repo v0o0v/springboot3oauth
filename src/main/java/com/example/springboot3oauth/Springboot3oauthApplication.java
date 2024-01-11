067436db5f2341e191f67f40c0269b85b9b93a2a
김민석 <v0o0v@daum.net>
2024-01-11 16:55:06 +0900

    Initial commit

---
+++ src/main/java/com/example/springboot3oauth/Springboot3oauthApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot3oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3oauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot3oauthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot3oauth/Springboot3oauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot3oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3oauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
